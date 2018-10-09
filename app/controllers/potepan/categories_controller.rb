0b22f6ffba56e547d899f79f7ad816fa77bb3378
Kojima Naoya <naoyak1989@gmail.com>
2018-10-09 21:07:15 +0900

    Fix categories_controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,9 +1,11 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @taxonomies = Spree::Taxonomy.includes(:taxons)
-    @products = @taxon.products
-    @taxons = @taxon.leaves
+    #@taxonomies = Spree::Taxonomy.includes(:taxons)
+    #@products = @taxon.products
+    @products = Spree::Product.includes(:taxons).in_taxon(@taxon)
+    #@taxons = @taxon.leaves
+    #@products_conut = Spree::Product.in_taxon(@taxon).count
     #@taxonomies = @taxon.taxonomy_id
     #@taxons = @taxon.leaves
     #@taxon_products = @taxons.ids.to_a
