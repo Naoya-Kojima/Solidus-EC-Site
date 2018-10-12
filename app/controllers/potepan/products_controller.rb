eeddb12c1b33869cb0e2557aff10689f5a740d58
Kojima Naoya <naoyak1989@gmail.com>
2018-10-12 15:11:20 +0900

    Fix products/show.html.erb

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,5 +2,7 @@ class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
+    @taxon = Spree::Taxon.find(params[:id])
+    @products = Spree::Product.includes(:taxons).in_taxon(@taxon)
   end
 end
