938bba432072d5bf6ee7e19db4f47595e77976f9
Kojima Naoya <naoyak1989@gmail.com>
2018-10-15 16:06:58 +0900

    Improve products_categories behavior

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,7 +2,5 @@ class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
-    @taxon = Spree::Taxon.find(params[:id])
-    @products = Spree::Product.includes(:taxons).in_taxon(@taxon)
   end
 end
