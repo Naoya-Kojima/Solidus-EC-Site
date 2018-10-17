87b1e3f1b04e5b54f68f47bdb377ca7cfed38565
Kojima Naoya <naoyak1989@gmail.com>
2018-10-17 18:26:55 +0900

    Improve products/show related_product

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -3,6 +3,6 @@ class Potepan::ProductsController < ApplicationController
     @product = Spree::Product.find(params[:id])
     @images = @product.images
     @taxon = Spree::Taxon.find(@product.taxon_ids.first)
-    @relative_products = Spree::Product.includes(:taxons).in_taxon(@taxon)
+    @related_products = Spree::Product.includes(:taxons).in_taxon(@taxon)
   end
 end
