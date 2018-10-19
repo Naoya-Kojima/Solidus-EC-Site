ae77293a83169f89e413edd21872139bdfe9b30d
Kojima Naoya <naoyak1989@gmail.com>
2018-10-19 15:42:49 +0900

    Merge kadai3 into kadai4

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,8 +1,8 @@
 class Potepan::ProductsController < ApplicationController
-  def show
+	def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
     @taxon = Spree::Taxon.find(@product.taxon_ids.first)
     @related_products = Spree::Product.includes(:taxons).in_taxon(@taxon)
-  end
+	end
 end
