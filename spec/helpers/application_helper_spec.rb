ad739582358fe8252912da8a1258a01a3a352c6d
Kojima Naoya <naoyak1989@gmail.com>
2018-10-20 18:00:20 +0900

    Remove some dead line products_controller_spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-
   describe 'full_title method' do
-
     context "when page_title is empty" do
       it "display only base_title" do
         expect(full_title('')).to eq('BIGBAG Store')
