c391bd4af15b12e57d9214a9f5233629415bbc3c
Kojima Naoya <naoyak1989@gmail.com>
2018-10-25 16:03:38 +0900

    Add bullet to gemfile and Improve categories_controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.includes(:taxons)
-    @products = Spree::Product.includes(:taxons).in_taxon(@taxon)
+    @products = Spree::Product.includes(master: [:images, :default_price]).in_taxon(@taxon)
   end
 end
