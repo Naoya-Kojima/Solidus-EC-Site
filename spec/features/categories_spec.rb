67d2c7e41a662c2de4acc7e0649f93a1eef904ed
Kojima Naoya <naoyak1989@gmail.com>
2018-11-02 18:11:51 +0900

    Add spec/feature/categories_spec

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,43 +1,30 @@
 require 'rails_helper'
 
 RSpec.feature "Potepan::Categories", type: :feature do
-  Spree::Config[:currency] = "JPY"
-
-  given(:taxonomy) { create(:taxonomy, name: 'taxonomy_name') }
-  given(:root) { taxonomy.root }
-  given(:taxon) do
-    products = []
-    products << create(:product, id: 101, name: "product_name_1", price: "77.77")
-    products << create(:product, id: 102, name: "product_name_2", price: "88.88")
-    products << create(:product, id: 103, name: "product_name_3", price: "99.99")
-    create(:taxon, id: 201, name: "taxon_categories", products: products, parent: root, taxonomy: taxonomy)
+  given(:taxonomy) { create(:taxonomy, name: "Category") }
+  given(:super_bag) { taxonomy.root.children.create(name: "Super Bag") }
+  given(:bag) { super_bag.children.create(name: "Bag") }
+  given!(:product) do
+    product = create(:product, name: "Rails Bag")
+    product.taxons << bag
+  end
+  background do
+    visit potepan_category_path(bag.id)
+  end
+  scenario "can see a taxon's products" do
+    expect(page).to have_content("Rails Bag")
+    expect(page).to have_content bag.products.name.first
   end
 
-  scenario "categories view test", js: true do
-    visit "/potepan/categories/#{taxon.id}"
+  scenario "can see mutually related categories and product page " do
+    click_on "Rails Bag"
+    expect(page).to have_current_path(potepan_product_path(bag.products.ids.first))
+    click_on "一覧ページへ戻る"
+    expect(page).to have_current_path(potepan_category_path(bag.id))
+  end
 
-    # TITLE
-    expect(page).to have_title "taxon_categories-BIGBAG Store"
-
-    # LIGHT SECTION
-    expect(page).to have_content "TAXON_CATEGORIES"
-
-    # サイドバーのカテゴリー(taxonomy)
-    expect(page).to have_link "taxonomy_name", href: "javascript:;"
-
-    # カテゴリークリック(taxon表示)
-    click_link "taxonomy_name"
-
-    # サイドバーカテゴリー(taxon)
-    expect(page).to have_link "taxon_categories", href: "/potepan/categories/201"
-    expect(page).to have_content "(3)"
-
-    # 表示商品
-    expect(page).to have_link "product_name_1", href: "/potepan/products/101"
-    expect(page).to have_content "$77.77"
-    expect(page).to have_link "product_name_2", href: "/potepan/products/102"
-    expect(page).to have_content "$88.88"
-    expect(page).to have_link "product_name_3", href: "/potepan/products/103"
-    expect(page).to have_content "$99.99"
+  scenario "can see index page" do
+    click_on 'Logo'
+    expect(page).to have_current_path(potepan_index_path)
   end
 end
