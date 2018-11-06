168790f411edc69614a81d37c7aa36a6eec66ae8
Kojima Naoya <naoyak1989@gmail.com>
2018-11-06 19:22:16 +0900

    Improve categories_controller_spec and feature/categories_spec

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
   describe "GET #show" do
+    let(:taxonomies) do
+      create_list(:taxonomy, 2) do |taxonomy|
+        taxon.taxonomy << taxonomy
+      end
+    end
     let(:taxon) { create(:taxon) }
-    let(:taxonomies) { Spree::Taxonomy.includes(:taxons) }
     let(:products) do
       create_list(:product, 5) do |product|
         product.taxons << taxon
@@ -22,12 +26,12 @@ RSpec.describe Potepan::CategoriesController, type: :controller do
       expect(response).to render_template :show
     end
 
-    it "assigns @taxon" do
-      expect(assigns(:taxon)).to eq taxon
+    it "assigns @taxonomies" do
+      expect(assigns(:taxonomies)).to match_array taxon.taxonomy
     end
 
-    it "assigns @taxonomies" do
-      expect(assigns(:taxonomies)).to eq taxonomies
+    it "assigns @taxon" do
+      expect(assigns(:taxon)).to eq taxon
     end
 
     it "assigns @products" do
