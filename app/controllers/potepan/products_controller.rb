d822cfb5d2a1e5b5ea62accc1093fc78fa83bef3
Kojima Naoya <naoyak1989@gmail.com>
2018-11-07 14:49:52 +0900

    Improve products_controller_spec and feature/products_spec

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,7 +1,12 @@
 class Potepan::ProductsController < ApplicationController
+  RELATED_PRODUCTS_LIMIT = 8
+
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
-    @related_products = Spree::Product.related_products(@product).excludes(@product)
+    @related_products = Spree::Product.
+      related_products(@product).
+      limit(RELATED_PRODUCTS_LIMIT).
+      excludes(@product)
   end
 end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,34 +1,47 @@
 require 'rails_helper'
 
 RSpec.feature "Potepan::Products", type: :feature do
-  given(:taxon) { create :taxon, name: "Kojima" }
-  given(:bag_1) { create(:product, taxons: [taxon]) }
-  given!(:bags) { create_list(:product, 10, name: "KOJIMADAYO", taxons: [taxon]) }
-  given(:other_taxon) { create :taxon}
+  given(:cap_taxon) { create :taxon, name: "Cap" }
+  given(:potepan_cap) { create(:product, name: "Potepan Cap", taxons: [cap_taxon]) }
+  given(:tech_cap) { create(:product, name: "Tech Cap", taxons: [cap_taxon]) }
+  given!(:caps) { create_list(:product, 10, name: "Ruby Cap", taxons: [cap_taxon]) }
+  given(:other_taxon) { create :taxon }
   given(:shirt) { create :product, taxons: [other_taxon] }
 
   background do
-    visit potepan_product_path(bag_1.id)
+    visit potepan_product_path(potepan_cap.id)
   end
 
   scenario "can see a products" do
-    visit potepan_product_path(bag_1.id)
+    visit potepan_product_path(potepan_cap.id)
     expect(page).to have_content "一覧ページへ戻る"
-    expect(page).to have_content bag_1.name
-    expect(page).to have_content bag_1.price
-    expect(page).to have_content bag_1.description
+    expect(page).to have_content potepan_cap.name
+    expect(page).to have_content potepan_cap.price
+    expect(page).to have_content potepan_cap.description
+  end
+
+  scenario "can see mutually related categories and product page" do
+    click_on "一覧ページへ戻る"
+    expect(page).to have_current_path(potepan_category_path(cap_taxon.id))
+    click_on "Potepan Cap"
+    expect(page).to have_current_path(potepan_product_path(potepan_cap.id))
+    visit potepan_product_path(tech_cap.id)
+    click_on "一覧ページへ戻る"
+    expect(page).to have_current_path(potepan_category_path(cap_taxon.id))
+    click_on "Tech Cap"
+    expect(page).to have_current_path(potepan_product_path(tech_cap.id))
   end
 
   scenario "can see a related_products" do
-    visit potepan_product_path(bag_1.id)
-    expect(page).to have_content bag_1.name
-    expect(page).to have_selector 'h5', text: "KOJIMADAYO"
+    visit potepan_product_path(potepan_cap.id)
+    expect(page).to have_content potepan_cap.name
+    expect(page).to have_selector 'h5', text: "Ruby Cap"
   end
 
   scenario "related_product count is 8" do
-    visit potepan_product_path(bag_1.id)
-    expect(page).to have_content bag_1.name
-    expect(page).to have_selector 'h5', text: "KOJIMADAYO", count: 8
+    visit potepan_product_path(potepan_cap.id)
+    expect(page).to have_content potepan_cap.name
+    expect(page).to have_selector 'h5', text: "Ruby Cap", count: 8
   end
 
   scenario "not show a related_products if related_product has no products" do
