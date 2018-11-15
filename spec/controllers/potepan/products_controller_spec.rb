f99cca21aeee873f5b146b0f364b5cacd742360e
Kojima Naoya <naoyak1989@gmail.com>
2018-11-15 20:39:07 +0900

    Improve products_controller_spec and feature/product_spec

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -4,30 +4,54 @@ RSpec.describe Potepan::ProductsController, type: :controller do
   describe 'GET #show' do
     let(:bag) { create(:taxon) }
     let(:backpack) { create(:product, taxons: [bag]) }
-    let!(:related_products) { create_list(:product, 8, taxons: [bag]) }
+    let!(:related_products) { create_list(:product, 4, taxons: [bag]) }
+    let(:other_taxon) { create(:taxon) }
+    let!(:other_products) { create_list(:product, 12, taxons: [other_taxon]) }
 
     before do
       get :show, params: { id: backpack.id }
     end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+    describe 'GET #show' do
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
 
-    it "renders show template" do
-      expect(response).to render_template :show
-    end
+      it "renders show template" do
+        expect(response).to render_template :show
+      end
 
-    it "assigns @product" do
-      expect(assigns(:product)).to eq backpack
-    end
+      describe "@related_products" do
+        context "when related_products" do
+          it "assigns @product" do
+            expect(assigns(:product)).to eq backpack
+          end
 
-    it "assigns @related_products" do
-      expect(assigns(:related_products)).to match_array related_products
-    end
+          it "assigns @related_products" do
+            expect(assigns(:related_products)).to match_array related_products
+          end
 
-    it "assigns @related_products size" do
-      expect(assigns(:related_products).size).to eq related_products.size
+          context "when related_products have less than 8 products" do
+            it "assigns @related_products size is 4" do
+              expect(assigns(:related_products).size).to eq 4
+            end
+          end
+
+          context "related_products have more than 8 products " do
+            let!(:related_products_6) { create_list(:product, 6, taxons: [bag]) }
+
+            it "assigns @related_products size is 8" do
+              expect(assigns(:related_products).size).to eq 8
+            end
+          end
+        end
+
+        context "when no related_products" do
+          it "not assigns other_products" do
+            expect(assigns(:related_products)).not_to match_array other_products
+          end
+        end
+      end
     end
   end
 end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -4,7 +4,16 @@ RSpec.feature "Potepan::Products", type: :feature do
   given(:cap_taxon) { create :taxon, name: "Cap" }
   given(:potepan_cap) { create(:product, name: "Potepan Cap", taxons: [cap_taxon]) }
   given(:tech_cap) { create(:product, name: "Tech Cap", taxons: [cap_taxon]) }
-  given!(:caps) { create_list(:product, 10, name: "Ruby Cap", taxons: [cap_taxon]) }
+  given!(:caps) { create(:product, name: "Ruby Cap", taxons: [cap_taxon]) }
+  given!(:caps_1) { create(:product, name: "Ruby Cap 1", taxons: [cap_taxon]) }
+  given!(:caps_2) { create(:product, name: "Ruby Cap 2", taxons: [cap_taxon]) }
+  given!(:caps_3) { create(:product, name: "Ruby Cap 3", taxons: [cap_taxon]) }
+  given!(:caps_4) { create(:product, name: "Ruby Cap 4", taxons: [cap_taxon]) }
+  given!(:caps_5) { create(:product, name: "Ruby Cap 5", taxons: [cap_taxon]) }
+  given!(:caps_6) { create(:product, name: "Ruby Cap 6", taxons: [cap_taxon]) }
+  given!(:caps_7) { create(:product, name: "Ruby Cap 7", taxons: [cap_taxon]) }
+  given!(:caps_8) { create(:product, name: "Ruby Cap 8", taxons: [cap_taxon]) }
+  given!(:caps_9) { create(:product, name: "Ruby Cap 9", taxons: [cap_taxon]) }
   given(:other_taxon) { create :taxon }
   given(:shirt) { create(:product, taxons: [other_taxon]) }
 
@@ -35,13 +44,13 @@ RSpec.feature "Potepan::Products", type: :feature do
   scenario "can see a related_products" do
     visit potepan_product_path(potepan_cap.id)
     expect(page).to have_content potepan_cap.name
-    expect(page).to have_selector ".productCaption", text: "Ruby Cap"
+    expect(page).to have_selector ".productBox", text: "Ruby Cap"
   end
 
   scenario "related_product count is 8" do
     visit potepan_product_path(potepan_cap.id)
     expect(page).to have_content potepan_cap.name
-    expect(page).to have_selector ".productCaption", text: "Ruby Cap", count: 8
+    expect(page).to have_selector ".productBox", count: 8
   end
 
   scenario "not show a related_products if related_product has no products" do
