7fefd1e62fe048fe30c77b8ae6ece16fd8e3ab33
Kojima Naoya <naoyak1989@gmail.com>
2018-11-15 20:39:07 +0900

    Improve products_controller_spec and feature/product_spec

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,8 +2,9 @@ Spree::Product.class_eval do
   scope :related_products, lambda { |product|
     includes_images_price.
       joins(:taxons).
-      where(spree_taxons: { id: product.taxon_ids })
+      where(spree_taxons: { id: product.taxon_ids }).
+      distinct
   }
   scope :includes_images_price, -> { includes(master: [:images, :default_price]) }
-  scope :excludes, ->(product) { where.not(id: product.id).distinct }
+  scope :excludes, ->(product) { where.not(id: product) }
 end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -4,11 +4,14 @@ RSpec.feature "Potepan::Products", type: :feature do
   given(:cap_taxon) { create :taxon, name: "Cap" }
   given(:potepan_cap) { create(:product, name: "Potepan Cap", taxons: [cap_taxon]) }
   given(:tech_cap) { create(:product, name: "Tech Cap", taxons: [cap_taxon]) }
-  given!(:caps) { create_list(:product, 10, name: "Ruby Cap", taxons: [cap_taxon]) }
+  given!(:caps) { create(:product, name: "Ruby Cap", taxons: [cap_taxon]) }
   given(:other_taxon) { create :taxon }
   given(:shirt) { create(:product, taxons: [other_taxon]) }
 
   background do
+    (1..9).each do |i|
+      create(:product, name: "Ruby Cap #{i}", taxons: [cap_taxon])
+    end
     visit potepan_product_path(potepan_cap.id)
   end
 
@@ -35,13 +38,13 @@ RSpec.feature "Potepan::Products", type: :feature do
   scenario "can see a related_products" do
     visit potepan_product_path(potepan_cap.id)
     expect(page).to have_content potepan_cap.name
-    expect(page).to have_selector ".productCaption", text: "Ruby Cap"
+    expect(page).to have_selector ".productBox", text: "Ruby Cap"
   end
 
   scenario "related_product count is 8" do
     visit potepan_product_path(potepan_cap.id)
     expect(page).to have_content potepan_cap.name
-    expect(page).to have_selector ".productCaption", text: "Ruby Cap", count: 8
+    expect(page).to have_selector ".productBox", count: 8
   end
 
   scenario "not show a related_products if related_product has no products" do
