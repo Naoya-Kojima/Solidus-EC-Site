522bd24cba6c1a5835bc092e03ca16cc9c9edf73
Kojima Naoya <naoyak1989@gmail.com>
2018-11-19 09:44:58 +0900

    Fix layouts/_header and Delete instance variable

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,4 @@
 class Potepan::ProductsController < ApplicationController
-  before_action :product_taxonomy
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
